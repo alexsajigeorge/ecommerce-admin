e779776c4a6b9acb2098555e32c23f3d2cfdd21e
Alex Saji <alex.saji@honorfx.com>
2023-12-11 12:11:13 +0400

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from 'next/font/google'
-
 import { ModalProvider } from '@/providers/modal-provider'
-
 import ToastProvider from '@/providers/toast-provider'
 import { ThemeProvider } from '@/providers/theme-provider'
-
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
